700d95c1fa47002c11b69b185ff35c16426f4e1d
Corey H <c.l.halicke@gmail.com>
2019-02-12 17:40:34 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(sentence)
   array = sentence.split
   rev_array = []
   array.each do | word |
-  rev_array.push(word.reverse)
-  return rev_array.join
+    rev_array.push(word.reverse)
+    return rev_array.join
   end
 end
